cdff363320aa007625c7a4a8531ccf13d3531918
ianschu <ianschu7@gmail.com>
2018-12-10 20:17:33 -0500

    basic functions finished

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -21,12 +21,12 @@ $(document).ready(function () {
             }
         }
     };
-    
 
-    // put the original buttons up
+    // adds the first three buttons
     object.createButtons();
 
-    $("#addAnimal").on("click", function(event) {
+    // event for adding new buttons
+    $("body").on("click", "#addAnimal", function (event) {
         // prevents submit from submitting
         event.preventDefault();
         // log array
@@ -34,49 +34,76 @@ $(document).ready(function () {
         // set animal name to search value
         var animalName = $("#gif-search").val().trim();
         // checks for blank searches
-        if(animalName.length === 0){
+        if (animalName.length === 0) {
             console.log("blank search")
         } else {
-        console.log(animalName);
-        // add search val to animal array
-        object.animalArray.push(animalName);
-        // create buttons
-        object.createButtons();
+            console.log(animalName);
+            // add search val to animal array
+            object.animalArray.push(animalName);
+            // create buttons
+            object.createButtons();
         }
-    })
-    $(".animalButton").on("click", function(){
+
+    });
+    // event for adding gifs
+    $("header").on("click", ".animalButton", function () {
         console.log(this, $(this).attr("data-name"));
         // get "data-name" of button (same as text on button)
         name = $(this).attr("data-name");
-        var rating=null;
         var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=UDbW5pMSEMGrWLs5VtOojxU0gy7XVEi4&q="
-        // search query
-        +name
-        // number of results returned
-        +"&limit=10"
-        +"&offset=0"
-        // rating
-        +"&rating=G"
-        // language
-        +"&lang=en";
+            // search query
+            + name
+            // number of results returned
+            + "&limit=10"
+            + "&offset=0"
+            // rating
+            // + "&rating="
+            // + rating
+            // language
+            + "&lang=en";
         // ajax request
         $.ajax({
             url: queryURL,
             method: "GET"
-        }).then(function(response) {
+        }).then(function (response) {
             console.log(response);
-            for (var i=0; i<response.data.length; i++){
-            console.log(response.data[i].images.fixed_height.url);
-            newImg =  $("<img>").attr("src", response.data[i].images.fixed_height_still.url);
-            newImg.addClass("gif");
-            $("#left").prepend(newImg);
+            for (var i = 0; i < response.data.length; i++) {
+                console.log(response.data[i].images.fixed_height.url, response.data[i].rating);
+                // create img element for gif
+                newImg = $("<img>");
+                // set src to a still of gif
+                newImg.attr("src", response.data[i].images.fixed_height_still.url);
+                // set attributes for still and animated versions of gif
+                newImg.attr("data-still", response.data[i].images.fixed_height_still.url);
+                newImg.attr("data-animate", response.data[i].images.fixed_height.url);
+                newImg.attr("state", "still");
+                // add class for click event
+                newImg.addClass("gif");
+                // create p element for rating
+                newP = $("<p>");
+                // set text of p
+                newP.text("Rating: "+(response.data[i].rating).toUpperCase());
+                // prepend images
+                $("#left").prepend(newImg).prepend(newP);
+                // prepend rating
             }
-           
         })
-        $(".gif").on("click", function(){
-            console.log("gif clicked");
-        })
+    })
+    // event for playing/pausing gifs
+    $("body").on("click", ".gif", function () {
+        console.log("gif clicked");
+        // if state is set to still src will be set to the animated url
+        if ($(this).attr("state") === "still") {
+            animate = $(this).attr("data-animate");
+            $(this).attr("src", animate);
+            $(this).attr("state", "animate");
+        }
+        // else src will be set to the still url
+        else {
+            still = $(this).attr("data-still");
+            $(this).attr("src", still);
+            $(this).attr("state", "still");
+        }
     });
-
 
 });
